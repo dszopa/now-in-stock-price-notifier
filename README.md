f58b207346d9e1d6764e63566916c956aaaa35c6
dszopa <szopanator@gmail.com>
2018-02-24 12:17:12 -0600

    Add support for basic flags

--- README.md
+++ README.md
@@ -9,13 +9,14 @@ A script to automatically send you an email if now-in-stock has an item you want
 ```bash
 npm install
 cd src
-node index.js <price> <url>
+node index.js <price> <url> <flags>
 ```
 
 ### Example Call:
 ```bash
-node index.js 750 "https://www.nowinstock.net/computers/videocards/nvidia/gtx1080ti/"
+node index.js 750 "https://www.nowinstock.net/computers/videocards/nvidia/gtx1080ti/" -R -I 500
 ```
 
 ## TODO
-- Talk about auth.js (maybe make it work through command line)
+- Talk about auth.js (maybe make it work through command line, json, or flags)
+- Add flags to README
--- src/index.js
+++ src/index.js
@@ -4,93 +4,117 @@ const util = require("./util");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-if (process.argv.length !== 4) {
+function displayCorrectUsage() {
   console.log(
     "Incorrect program usage.\n" +
       "Correct Usage:\n" +
-      "\tnode index.js <price> <url>\n" +
+      "\tnode index.js <price> <url> <flags>\n" +
       "Correct Usage Example:\n" +
-      '\tnode index.js 750 "https://www.nowinstock.net/computers/videocards/nvidia/gtx1080ti/"'
+      '\tnode index.js 750 "https://www.nowinstock.net/computers/videocards/nvidia/gtx1080ti/" -R -I 500'
   );
   process.exit(1);
 }
 
+// Verify correct argument length
+if (process.argv.length < 4) {
+  displayCorrectUsage();
+}
+
 const trackingPrice = process.argv[2];
 const trackingUrl = process.argv[3];
+const loop = process.argv.includes("--repeat") || process.argv.includes("-R");
 let trackingRows = [];
+let interval = 300000; // 5 Minutes
 
-// "Main"
-axios.default
-  .get(trackingUrl)
-  .then(response => {
-    trackingRows = [];
-    const $ = cheerio.load(response.data);
+// TODO: if specifying interval, make sure that repeat is given.
+if (process.argv.includes("--interval")) {
+  interval = process.argv[process.indexOf("--interval") + 1];
+} else if (process.argv.includes("-I")) {
+  interval = process.argv[process.argv.indexOf("-I") + 1];
+}
 
-    const table = $("#trackerContent > #data > table > tbody");
-    const rows = table.find("tr");
+// Verify values for trackingPrice & trackingUrl
+if (!trackingPrice || Number.isNaN(Number(trackingPrice)) || !trackingUrl) {
+  displayCorrectUsage();
+}
 
-    rows.each((index, element) => {
-      if (element.childNodes.length < 5) {
-        return;
-      }
+function handleResponse(response) {
+  trackingRows = [];
+  const $ = cheerio.load(response.data);
 
-      let name = "";
-      let url = "";
-      let store = "";
-      let status = "";
-      let lastPrice = null;
-      let lastStock = "";
+  const table = $("#trackerContent > #data > table > tbody");
+  const rows = table.find("tr");
 
-      const urlElement = element.childNodes[1].childNodes[0];
-      if (urlElement.attribs != null) {
-        url = urlElement.attribs.href;
-      }
+  rows.each((index, element) => {
+    if (element.childNodes.length < 5) {
+      return;
+    }
 
-      const nameElement = element.childNodes[1].childNodes[0].firstChild;
-      if (nameElement != null) {
-        name = util.parseName(nameElement.data);
-        store = util.parseStore(nameElement.data);
-      }
+    let name = "";
+    let url = "";
+    let store = "";
+    let status = "";
+    let lastPrice = null;
+    let lastStock = "";
 
-      const statusElement = element.childNodes[2].firstChild;
-      if (statusElement != null) {
-        status = statusElement.data;
-      }
+    const urlElement = element.childNodes[1].childNodes[0];
+    if (urlElement.attribs != null) {
+      url = urlElement.attribs.href;
+    }
 
-      const lastPriceElement = element.childNodes[3].firstChild;
-      if (lastPriceElement != null) {
-        lastPrice = util.moneyStringToNumber(lastPriceElement.data);
-      }
+    const nameElement = element.childNodes[1].childNodes[0].firstChild;
+    if (nameElement != null) {
+      name = util.parseName(nameElement.data);
+      store = util.parseStore(nameElement.data);
+    }
 
-      const lastStockElement = element.childNodes[5].firstChild;
-      if (lastStockElement != null) {
-        lastStock = lastStockElement.data;
-      }
+    const statusElement = element.childNodes[2].firstChild;
+    if (statusElement != null) {
+      status = statusElement.data;
+    }
 
-      if (
-        name !== "" &&
-        store !== "" &&
-        status !== "" &&
-        url !== "" &&
-        lastPrice != null &&
-        !lastPrice.isNan &&
-        lastStock !== ""
-      ) {
-        trackingRows.push(
-          new TrackingRow(index, name, store, url, status, lastPrice, lastStock)
-        );
-      }
-    });
+    const lastPriceElement = element.childNodes[3].firstChild;
+    if (lastPriceElement != null) {
+      lastPrice = util.moneyStringToNumber(lastPriceElement.data);
+    }
 
-    trackingRows.forEach(trackingRow => {
-      if (
-        trackingRow.price < trackingPrice &&
-        trackingRow.status === "In Stock"
-      ) {
-        email.sendEmail(trackingRow, trackingPrice, trackingUrl);
-      }
-    });
-  })
-  .catch(error => {
-    console.log(error);
+    const lastStockElement = element.childNodes[5].firstChild;
+    if (lastStockElement != null) {
+      lastStock = lastStockElement.data;
+    }
+
+    if (name && store && status && url && lastPrice && lastStock) {
+      trackingRows.push(
+        new TrackingRow(index, name, store, url, status, lastPrice, lastStock)
+      );
+    }
   });
+
+  trackingRows.forEach(trackingRow => {
+    if (
+      trackingRow.price < trackingPrice &&
+      trackingRow.status === "In Stock"
+    ) {
+      email.sendEmail(trackingRow, trackingPrice, trackingUrl);
+    }
+  });
+}
+
+function handleError(error) {
+  console.log(`Request errored: ${error}`);
+}
+
+function main() {
+  console.log(`Requesting ${trackingUrl} for prices below ${trackingPrice}`);
+  axios.default
+    .get(trackingUrl)
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+if (loop === true) {
+  main();
+  setInterval(main, interval);
+} else {
+  main();
+}
